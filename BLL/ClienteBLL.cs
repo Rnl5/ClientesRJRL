d0fdab3d8f94eb90f2ff9d432b7fdbc0785d25ee
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-09-07 19:34:03 -0400

    Se agregaron los metodos de ClienteBLL

--- BLL/ClienteBLL.cs
+++ BLL/ClienteBLL.cs
@@ -1,5 +1,62 @@
+using ClientesRJRL.DAL;
+using ClientesRJRL.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace ClientesRJRL.BLL;
 
 public class ClienteBLL{
-    
+    private readonly ClientesContext _context;
+
+    public ClienteBLL(ClientesContext context){
+        _context = context;
+    }
+
+    public bool YaExiste(int ClienteId){
+        return _context.Clientes.Any(c => c.ClienteId == ClienteId);
+    }
+
+    public bool Insertar(Clientes cliente)
+    {
+        _context.Clientes.Add(cliente);
+        return _context.SaveChanges() > 0;
+    }
+
+    public bool Modificar(Clientes cliente){
+        _context.Entry(cliente).State = EntityState.Modified;
+        return _context.SaveChanges() > 0;
+    }
+
+    public bool Guardar(Clientes cliente){
+        if(!YaExiste(cliente.ClienteId))
+        {
+            return this.Insertar(cliente);
+        }
+        else
+        {
+            return this.Modificar(cliente);
+        }
+    }
+
+    public bool Eliminar(Clientes cliente)
+    {
+        _context.Entry(cliente).State = EntityState.Deleted;
+        return _context.SaveChanges() > 0;
+    }
+
+    public Clientes? Buscar(int ClienteId)
+    {
+        return _context.Clientes
+            .Where(c => c.ClienteId == ClienteId)
+            .AsNoTracking()
+            .SingleOrDefault();
+    }
+
+    public List<Clientes> GetClientes()
+    {
+        return _context.Clientes
+            .AsNoTracking()
+            .ToList();
+    }
+
+
 }
